9651b3692bd802040a4302f19dbe855584436986
Ravi Pratap Singh <ravipratap@lpu.in>
2026-05-08 10:23:22 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://pawanpc044_db_user:U2PuRhNnQFoem99V@cluster0.xxxxx.mongodb.net/testdb?retryWrites=true&w=majority"
+);
+
+const User = mongoose.model("User", {
+  name: String
+});
+
+app.post("/users", async (req, res) => {
+  const user = new User({ name: req.body.name });
+  await user.save();
+  res.send(user);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
+});
